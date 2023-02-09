56899708d0868a46cc53c91e335d9fdf6143e203
dennyArfansyah <dennyarfansyah@gmail.com>
2023-02-09 23:25:48 +0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Denny Arfansyah on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
